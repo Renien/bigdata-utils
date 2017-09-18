bf670ba57094182e5078b902c7ca3b464dd067fb
Renien <renien.john@gmail.com>
2017-09-18 10:03:52 +0530

    modfiifed to accept bash parameters

--- bash/hadoop/hbase.sh
+++ bash/hadoop/hbase.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # This will help read row keys and execute hbase get key command
-for line in $(hadoop fs -cat /staged/reports/propensity_coverage/no_prop_atg_id_report/missing_ids/part-00000)
+for line in $(hadoop fs -cat ${fileLocation})
 do
 	echo "$line"
-	rows=$(echo "get '$table', 'line', {COLUMN => ['rec:176'] }" | hbase shell | grep value | grep -o value=.* | cut -f2- -d'=')
+	rows=$(echo "get '${table}', 'line', {COLUMN => ['${colFamily}:${colQualifier}'] }" | hbase shell | grep value | grep -o value=.* | cut -f2- -d'=')
 	echo "The row value: $rows"
 done
