5878838fc843a3c38382554f2ef276c46f32f978
Renien <renien.john@gmail.com>
2017-09-23 10:54:27 +0530

    adding hive util. feature: to exatract hive table hdfs store location

---
+++ bash/hadoop/hive.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Load the until files
+scripts_path=$(dirname $(readlink -f "$0"))"/../"
+source "$scripts_path/utils/common-functions.sh"
+
+# Parse args
+read_args $@
+
+# validating args
+validate_variable_args "db" ${db}
+validate_variable_args "table" ${table}
+
+# Get the hive table hdfs store location
+location=$(hive -e "DESCRIBE FORMATTED ${db}.${table}" | grep Location:)
+
+# hive hdfs location
+info $location
