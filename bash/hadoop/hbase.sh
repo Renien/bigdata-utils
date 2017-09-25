e8590d19f919b14b5354cd0c59c3e984b832cda1
Renien <renien.john@gmail.com>
2017-09-25 09:47:20 +0530

    modified the scripts to use the util functions

--- bash/hadoop/hbase.sh
+++ bash/hadoop/hbase.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
-# This will help read row keys and execute hbase get key command
+# Load the until files
+scripts_path=$(dirname $(readlink -f "$0"))"/../"
+source "$scripts_path/utils/common-functions.sh"
+
+# Parse args
+read_args $@
+
+# validating args
+validate_variable_args "fileLocation" ${fileLocation}
+
+: '
+This will help read row keys and execute 
+hbase get key command
+'
 for line in $(hadoop fs -cat ${fileLocation})
 do
-	echo "$line"
 	rows=$(echo "get '${table}', 'line', {COLUMN => ['${colFamily}:${colQualifier}'] }" | hbase shell | grep value | grep -o value=.* | cut -f2- -d'=')
-	echo "The row value: $rows"
+	info "${line} : ${rows}"
 done
--- bash/hadoop/hdfs.sh
+++ bash/hadoop/hdfs.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-echo "Parsing args.."
-for item in $@; do
-case $item in
-(*=*) eval $item;
-esac
-done
+# Load the until files
+scripts_path=$(dirname $(readlink -f "$0"))"/../"
+source "$scripts_path/utils/common-functions.sh"
+
+# Parse args
+read_args $@
+
+# validating args
+validate_variable_args "fileLocation" ${fileLocation}
+validate_variable_args "N" ${N}
 
 # Get few lines of HDFS data
 hadoop fs -cat ${fileLocation} | shuf -n ${N}
